f2f3a7fc6625018464a4def948a7ddd64757b9bd
Ivan_Surus <Ivan_Surus@emap.com>
2017-01-26 17:04:54 +0300

    Initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Ivan_Surus on 1/26/2017.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+    }
+
+}
